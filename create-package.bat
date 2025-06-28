6453183a7d75b5471814bd47def74abebb7b90d6
David Le Bansais <dlebansais@gmail.com>
2025-06-28 19:07:14 +0200

    Moved to x64.

--- create-package.bat
+++ create-package.bat
@@ -1,4 +1,4 @@
 nuget install Packager -DependencyVersion Highest -OutputDirectory packages
-packages\Packager.2.0.3\lib\net481\Packager.exe --prefix:dlebansais
+packages\Packager.2.1.0\lib\net481\Packager.exe --prefix:dlebansais
 nuget pack nuget\dlebansais.IDisposableAnalyzers.nuspec  
 pause
